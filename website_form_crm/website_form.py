c840169f28dfcc007fc5880b67c22797a5d45d4d
Anders Wallenquist <aw@leia.vertel.se>
2015-08-17 16:16:53 +0200

    New module website_form_crm

--- website_form_crm/website_form.py
+++ website_form_crm/website_form.py
@@ -31,29 +31,11 @@ import re
 import logging
 _logger = logging.getLogger(__name__)
 
-class website_form(http.Controller):
-
-    @http.route(['/form/<string:form>/add', ], type='http', auth="user", website=True)
-    def form_add(self, form=False,model_id=False,add_menu=0, thanks_template='website_form.thanks',**post):
-        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
-        form = request.env['form.form'].create({'name': form,'model_id': model_id,'body':'', 'thanks_template': thanks_template})
-        page = request.env['website'].new_page(form.name)
-        _logger.warning("This is form postnr %s" % (form))
+class website_form_crm(http.Controller):
 
 
-
-        if add_menu:
-            model, id  = request.registry["ir.model.data"].get_object_reference(request.cr, request.uid, 'website', 'main_menu')
-            request.registry['website.menu'].create(request.cr, request.uid, {
-                    'name': form.name,
-                    'url': "/form/" + form.name,
-                    'parent_id': id,
-                }, context=request.context)
-
-        return werkzeug.utils.redirect("/form/"+form.name)
-
-    @http.route(['/form/<string:form>', ], type='http', auth="public", website=True)
-    def form_view(self, form=False,**post):
+    @http.route(['/form/<string:form>/lead', ], type='http', auth="public", website=True)
+    def form_lead(self, form=False,**post):
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
         form = request.env['form.form'].search([('name','=', form)])
         if request.httprequest.method == 'POST':
@@ -82,39 +64,20 @@ class website_form(http.Controller):
         _logger.warning("This is form post %s %s" % (form, post))
         return request.render('website_form.form', {'form': form})
 
- 
-
-    @http.route(['/form/<string:form>/edit', ], type='http', auth="user", website=True)
-    def form_edit(self, form=False,**post):
-        cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
-        form = request.env['form.form'].search([('name','=', form)])
-        if request.httprequest.method == 'POST':
-
-            form_data = dict((field_name, post.pop('form.' + field_name))
-                for field_name in form.fields_get().keys() if post.get('form.'+field_name))
-
-            post_keys = list(key for key in post.keys())
-            _logger.warning("This is form post %s" % (post_keys))
-
-            form.write(form_data)
 
 
 
 
-        return request.render('website_form.edit', {'form': form})
-
-
-
-class form_form(models.Model):
-    _name = 'form.form'
-    _description = 'Simple Form'
-    _order = 'name'
-
-    name = fields.Char('Name',required=True)
-    model_id = fields.Many2one(comodel_name='ir.model',string='Model')
-    body = fields.Html('Body',sanitize=False)
-    thanks_url = fields.Char('Thanks Url', default="/page/website_form.thank_you")
-    auth_type = fields.Selection([('public','public'),('user','user'),('admin','admin')])
+#~ class form_form(models.Model):
+    #~ _name = 'form.form'
+    #~ _description = 'Simple Form'
+    #~ _order = 'name'
+#~ 
+    #~ name = fields.Char('Name',required=True)
+    #~ model_id = fields.Many2one(comodel_name='ir.model',string='Model')
+    #~ body = fields.Html('Body',sanitize=False)
+    #~ thanks_url = fields.Char('Thanks Url', default="/page/website_form.thank_you")
+    #~ auth_type = fields.Selection([('public','public'),('user','user'),('admin','admin')])
 
 
 
