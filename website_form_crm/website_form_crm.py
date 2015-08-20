d5471143801118666a256c238834f8d837764fc9
Anders Wallenquist <aw@leia.vertel.se>
2015-08-20 16:49:37 +0200

    Moves lead to opporttunity

--- website_form_crm/website_form_crm.py
+++ website_form_crm/website_form_crm.py
@@ -39,17 +39,11 @@ class website_form_crm(http.Controller):
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
         form = request.env['form.form'].search([('name', '=', form)])
         if not form:
-            _logger.warning("not form ")
-            return request.render('website.page_404', {})
-            
+            return request.render('website.page_404', {})            
         if not lead:
-            _logger.warning("not lead ")
             return request.render('website.page_404', {})
-
-        _logger.warning("Lead_id search: name: %s" % (lead.partner_name))
 
         if request.httprequest.method == 'POST':
-
             serial_fields = {}
             for key in post.keys():  # fields project.issue.description_1 .. nn
                 if re.match(".*_(\d+)", key):
@@ -59,7 +53,6 @@ class website_form_crm(http.Controller):
                         serial_fields[field_name].append(post.pop(key))
                     else:
                         serial_fields[field_name] = [post.pop(key)]
-
             form_data = dict((field_name, post.pop(form.model_id.model + '.' + field_name))  # fields project.issue.name
                              for field_name in request.env[form.model_id.model].fields_get().keys()
                              if post.get(form.model_id.model + '.' + field_name))
@@ -67,59 +60,15 @@ class website_form_crm(http.Controller):
                 if type(serial_fields[key]) is list:
                     form_data[key] = ', '.join(serial_fields[key])
             
-            _logger.warning("Form Data %s %s" % (form_data, post))
+            _logger.debug("Form Data %s %s" % (form_data, post))
             
-            
-            
+            partner = request.env['res.partner'].create({'is_company': True, 'name': form_data['partner_name'], 'email': form_data.get('email_from',''),  'phone': form_data.get('phone',''), 'ref': form_data.get('contact_name','') })
+            form_data['partner_id'] = partner.id
+            form_data['type'] = 'opportunity'
+            form_data['stage_id'] = request.env.ref('crm.stage_lead1').id
             lead.write(form_data)
             
             return werkzeug.utils.redirect(form.thanks_url)
 
-
-            
-
-            # form_data['type'] = 'opportunity'
-            # crm.stage_lead1
-            form_data['name'] = 'Canon 100D'
-            form_data['active'] = 1
-            form_data['type'] = 'opportunity'
-            # form_data['user_id'] = 1
-            # form_data['stage_id'] = 1
-            # form_data['section_id'] = 1
-            # form_data['company_id'] = 1
-            form_data['priority'] = '2'
-            # form_data['color'] = 0
-            form_data['partner_id'] = 7
-
-            # lead = request.env[form.model_id.model].write(form_data.pop('id'),form_data)
-            lead = request.env[form.model_id.model].browse(form_data.pop('id'))
-            _logger.warning("Form Data 2 %s %s" % (form_data, post))
-            lead.write(form_data)
-            # lead = request.env[form.model_id.model].create(form_data)
-            # lead.write({'partner_name': u'Christelle'})
-            # l2o = request.env['crm.lead2opportunity.partner'].with_context(active_id=lead.id,active_ids=[lead.id],active_model="crm.lead").action_apply()
-
-
-            _logger.warning("Form created object %s" % (lead))
-            # l2o = request.env['crm.lead2opportunity.partner'].with_context(active_id=lead_id, active_model="crm.lead").create({
-            #    'name': 'convert',
-            #    'action': 'create',
-            #    #~ 'user_id': lead.user_id,
-            #    #~ 'section_id': lead.section_id,
-            # })
-            # l2o.with_context(active_id=lead_id,active_ids=[lead_id], active_model="crm.lead").action_apply()
-
-
-        _logger.warning("This is form post form: %s post: %s lead: %s name: %s" % (form, post, lead, lead.partner_name))
         return request.render('website_form_crm.lead_form', {'form': form, 'lead': lead})
 
-    # ~ class form_form(models.Model):
-    # ~ _name = 'form.form'
-    # ~ _description = 'Simple Form'
-    # ~ _order = 'name'
-    # ~
-    # ~ name = fields.Char('Name',required=True)
-    # ~ model_id = fields.Many2one(comodel_name='ir.model',string='Model')
-    # ~ body = fields.Html('Body',sanitize=False)
-    # ~ thanks_url = fields.Char('Thanks Url', default="/page/website_form.thank_you")
-    # ~ auth_type = fields.Selection([('public','public'),('user','user'),('admin','admin')])
