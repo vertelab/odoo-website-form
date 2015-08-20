66e96249e43fe2ccbfbb7ea226fce53d77c03d53
HaojunZou <apollo_zhj@msn.com>
2015-08-20 14:35:46 +0200

    working on crm.lead, has problem with the access right

--- website_form_crm/website_form_crm.py
+++ website_form_crm/website_form_crm.py
@@ -29,33 +29,35 @@ import pytz
 import re
 
 import logging
+
 _logger = logging.getLogger(__name__)
 
+
 class website_form_crm(http.Controller):
-
-
-    @http.route(['/form/<string:form>/lead', ], type='http', auth="public", website=True)
-    def form_lead(self, form=False,lead_id=False,**post):
+    @http.route(['/form/<string:form>/lead', ], type='http', auth="user", website=True)
+    def form_lead(self, form=False, lead_id=False, **post):
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
-        form = request.env['form.form'].search([('name','=', form)])
-        if not form or not lead_id:
-            return request.render('website.page_404', {})        
-        
+        form = request.env['form.form'].search([('name', '=', form)])
+        if not form:
+            return request.render('website.page_404', {})
+
+        if not lead_id:
+            return request.render('website.page_404', {})
+
         if lead_id:
-            lead = request.env['crm.lead'].search([('id','=', lead_id)])
+            lead = request.env['crm.lead'].search([('id', '=', lead_id)])
+            if not lead:
+                return request.render('website.page_404', {})
+            _logger.warning("Lead_id search: name: %s lead_id: %s" % (lead.partner_name, lead_id))
         else:
             lead = False
- 
+
         if request.httprequest.method == 'POST':
 
-            
-            lead_id = int(post.pop('crm.lead.id'))
-            _logger.warning("id: %s" % lead_id)
-            
             form_data = {}
             for key in post.keys():  # fields project.issue.description_1 .. nn
-                if re.match(".*_(\d+)",key):
-                    (field_name,nr) = re.split('_',key,1)
+                if re.match(".*_(\d+)", key):
+                    (field_name, nr) = re.split('_', key, 1)
                     if form_data.get(field_name):
                         form_data[field_name].append(post.get(key))
                     else:
@@ -65,46 +67,57 @@ class website_form_crm(http.Controller):
                     form_data[key] = ', '.join(form_data[key])
 
             form_data = dict((field_name, post.pop(form.model_id.model + '.' + field_name))  # fields project.issue.name
-                for field_name in request.env[form.model_id.model].fields_get().keys()
+                             for field_name in request.env[form.model_id.model].fields_get().keys()
                              if post.get(form.model_id.model + '.' + field_name))
 
             _logger.warning("Form Data %s %s" % (form_data, post))
-            
-            
-            if form_data.get('id',False):
-                lead = request.env[form.model_id.model].write(form_data.pop('id'),form_data)
-                l2o = request.env['crm.lead2opportunity.partner'].with_context(active_id=lead.id,active_ids=[lead.id],active_model="crm.lead").action_apply()
-            else:    
+
+            if form_data.get('id', False):
+                # form_data['type'] = 'opportunity'
+                # crm.stage_lead1
+                form_data['name'] = 'Canon 100D'
+                form_data['active'] = 1
+                form_data['type'] = 'opportunity'
+                # form_data['user_id'] = 1
+                # form_data['stage_id'] = 1
+                # form_data['section_id'] = 1
+                # form_data['company_id'] = 1
+                form_data['priority'] = '2'
+                # form_data['color'] = 0
+                form_data['partner_id'] = 7
+
+                # lead = request.env[form.model_id.model].write(form_data.pop('id'),form_data)
+                lead = request.env[form.model_id.model].browse(form_data.pop('id'))
+                _logger.warning("Form Data 2 %s %s" % (form_data, post))
+                lead.write(form_data)
+                # lead = request.env[form.model_id.model].create(form_data)
+                # lead.write({'partner_name': u'Christelle'})
+                # l2o = request.env['crm.lead2opportunity.partner'].with_context(active_id=lead.id,active_ids=[lead.id],active_model="crm.lead").action_apply()
+            else:
                 lead = request.env[form.model_id.model].create(form_data)
-                
+
+
             _logger.warning("Form created object %s" % (lead))
-            #l2o = request.env['crm.lead2opportunity.partner'].with_context(active_id=lead_id, active_model="crm.lead").create({
+            # l2o = request.env['crm.lead2opportunity.partner'].with_context(active_id=lead_id, active_model="crm.lead").create({
             #    'name': 'convert',
             #    'action': 'create',
             #    #~ 'user_id': lead.user_id,
             #    #~ 'section_id': lead.section_id,
-			#})
-            #l2o.with_context(active_id=lead_id,active_ids=[lead_id], active_model="crm.lead").action_apply()
-			
+            # })
+            # l2o.with_context(active_id=lead_id,active_ids=[lead_id], active_model="crm.lead").action_apply()
+
             return werkzeug.utils.redirect(form.thanks_url)
-        
-        _logger.warning("This is form post %s %s" % (form, post))
-        return request.render('website_form.form', {'form': form,'lead':lead or False})
 
+        _logger.warning("This is form post form: %s post: %s lead: %s name: %s" % (form, post, lead, lead.partner_name))
+        return request.render('website_form_crm.lead_form', {'form': form, 'lead': lead})
 
-
-
-
-#~ class form_form(models.Model):
-    #~ _name = 'form.form'
-    #~ _description = 'Simple Form'
-    #~ _order = 'name'
-#~ 
-    #~ name = fields.Char('Name',required=True)
-    #~ model_id = fields.Many2one(comodel_name='ir.model',string='Model')
-    #~ body = fields.Html('Body',sanitize=False)
-    #~ thanks_url = fields.Char('Thanks Url', default="/page/website_form.thank_you")
-    #~ auth_type = fields.Selection([('public','public'),('user','user'),('admin','admin')])
-
-
-
+    # ~ class form_form(models.Model):
+    # ~ _name = 'form.form'
+    # ~ _description = 'Simple Form'
+    # ~ _order = 'name'
+    # ~
+    # ~ name = fields.Char('Name',required=True)
+    # ~ model_id = fields.Many2one(comodel_name='ir.model',string='Model')
+    # ~ body = fields.Html('Body',sanitize=False)
+    # ~ thanks_url = fields.Char('Thanks Url', default="/page/website_form.thank_you")
+    # ~ auth_type = fields.Selection([('public','public'),('user','user'),('admin','admin')])
